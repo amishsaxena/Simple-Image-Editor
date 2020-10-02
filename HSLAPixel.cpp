94bb5f7a949aa38b42059547e23587f132a248cf
Amish Ashish Saxena <71137235+amishsaxena@users.noreply.github.com>
2020-10-02 14:09:27 +0530

    Add files via upload

---
+++ HSLAPixel.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file HSLAPixel.cpp
+ * Implementation of the HSLAPixel class for use in with the PNG library.
+ */
+
+#include <cmath>
+#include <iostream>
+#include "HSLAPixel.h"
+using namespace std;
+
+namespace uiuc {
+}
---
+++ HSLAPixel.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+
+  // Put your HSLAPixel class definition here.
+  // (Remember to end it with a semicolon!)
+  class HSLAPixel
+  {
+  public:
+      double h,s,l,a;
+
+  };
+
+
+}
